dcc63f8e60942e244ed48891c14a221805a31e2d
Rayan84 <65650520+Rayan84@users.noreply.github.com>
2020-08-30 14:45:38 +0200

    Style works on Main and Squares

--- index.js
+++ index.js
@@ -2,19 +2,27 @@
  function testAlert(){
   alert("JS here!");
 }
-testAlert();
+//testAlert();
 
-// create squares
+                            // create squares
 function createSquares() {
-  //var squareTotalNumber = 100;
+                      //var squareTotalNumber = 100;
   var main = document.getElementById("main");
+  for (let i = 99; i > -1; i--){
   var square = document.createElement("DIV");
   var squareNumber = document.createElement("P");
-  for (let i = 0; i < 100; i++){
+  squareNumber.setAttribute("class", "squareNumber");
+  var squareNumberText = document.createTextNode(i);
+  squareNumber.appendChild(squareNumberText);
     square.setAttribute("class", "square");
+    square.appendChild(squareNumber);
     main.appendChild(square);
-    alert("square just created");
+    //alert(i);
   }
 }
-
 createSquares();
+
+                     // stones
+var stonePosition = 0;
+
+document.querySelectorAll(".square");
