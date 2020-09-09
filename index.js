18bcb4ff53e58b85667224cc8bb0c96e73d4bc22
Rayan84 <65650520+Rayan84@users.noreply.github.com>
2020-09-09 19:00:45 +0200

    Add second player Button style Switching between players

--- index.js
+++ index.js
@@ -1,68 +1,107 @@
-
-                            // create squares
 function createSquares() {
-                      //var squareTotalNumber = 100;
-  var main = document.getElementById("main");
-  for (let i = 99; i > -1; i--){
-  var square = document.createElement("DIV");
-  var squareNumber = document.createElement("P");
-  squareNumber.setAttribute("class", "squareNumber");
-  var squareNumberText = document.createTextNode(i);
-  squareNumber.appendChild(squareNumberText);
-    square.setAttribute("class", "square");
+  var main = document.getElementById('main');
+  for (let i = 99; i > -1; i--) {
+    var square = document.createElement('DIV');
+    var squareNumber = document.createElement('P');
+    squareNumber.setAttribute('class', 'squareNumber');
+    var squareNumberText = document.createTextNode(i);
+    squareNumber.appendChild(squareNumberText);
+    square.setAttribute('class', 'square');
     square.appendChild(squareNumber);
     main.appendChild(square);
-    //alert(i);
   }
 }
+
 createSquares();
 
+//                         dice
+var start = 0;
+var palyButton = document.getElementById('playButton');
+var diceUI;
+var dice;
+var interva;
 
-                              // dice
-
-                              // dice roll
-    function diceRoll(){
-         dice = Math.floor(Math.random() * 6) + 1;
-         console.log(dice);
-    }
-
-                           // dice UI
-
-function createDiceUI(){
-  var diceUI = document.createElement("IMG");
-  diceUI.setAttribute("class", "dice");
-  diceUI.setAttribute("src", "./media/dice.gif");
-  document.getElementById("main").appendChild(diceUI);
+//                  dice roll
+function diceRoll() {
+  playButton.removeAttribute('onclick');
+  console.log('onclick attribute removed');
+  console.log('Rolling Dice...');
+  dice = Math.floor(Math.random() * 6) + 1;
+  console.log(dice);
+  diceUI = document.createElement('IMG');
+  diceUI.setAttribute('class', 'dice');
+  console.log('diceUI set to gif');
+  document.getElementById('main').appendChild(diceUI);
+  diceUI.setAttribute('src', './media/dice.gif');
   setTimeout(function () {
-    diceRoll();
-  }, 5000);
+          deleteDiceUI();
+        }, 5000);
+
+  setTimeout(function () {
+        console.log('src, ./media/dice' + dice + '.svg');
+        diceUI.setAttribute('src', './media/dice' + dice + '.svg');
+        diceUI.style.display = 'block';
+      }, 5010);
+
+  setTimeout(function () {
+        deleteDiceUI();
+      }, 8000);
+
+  setTimeout(function () {
+          interva = setInterval(moveCounter, 500);
+        }, 8500);
 }
 
+//                                    dice UI
 
+function deleteDiceUI() {
+  diceUI.style.display = 'none';
+  console.log('deleted DiceUI');
+}
 
-                     // counters
+//                                counters
 
-var counterPosition = 99;
-var squaresArray = document.querySelectorAll(".square");
-var counter1 = document.createElement("IMG");
-    counter1.setAttribute("src", "./media/user_2.png");
-    counter1.setAttribute("class", "counter");
-    squaresArray[counterPosition].appendChild(counter1);
+var counter1Position = 99;
+var squaresArray = document.querySelectorAll('.square');
+var counter1 = document.createElement('IMG');
+counter1.setAttribute('src', './media/user_2.png');
+counter1.setAttribute('class', 'counter');
+squaresArray[counter1Position].appendChild(counter1);
 
-var counter2 = document.createElement("IMG");
-    counter2.setAttribute("src", "./media/superman.ico");
-    counter2.setAttribute("class", "counter");
-    squaresArray[counterPosition].appendChild(counter2);
+var counter2Position = 99;
+var counter2 = document.createElement('IMG');
+counter2.setAttribute('src', './media/superman.ico');
+counter2.setAttribute('class', 'counter');
+squaresArray[counter2Position].appendChild(counter2);
 
-var interva = setInterval(moveCounter, 500);
- function moveCounter(){
-   if(dice > 0){
-     counterPosition--;
-     dice--;
-     squaresArray[counterPosition].appendChild(counter1);
-   }else {
+var activePlayer = counter1;
+function moveCounter() {
+  if (dice > 0) {
+    if (activePlayer == counter1) {
+      counter1Position--;
+      dice--;
+      squaresArray[counter1Position].appendChild(counter1);
+      console.log('counter1 Position = ' + counter1Position);
+    }else {
+      counter2Position--;
+      dice--;
+      squaresArray[counter2Position].appendChild(counter2);
+      console.log('counter2 Position = ' + counter2Position);
+    }
 
-     clearInterval(interva)
+  }else {
+    if (activePlayer == counter1) {
+      activePlayer = counter2;
+      console.log('switching to Player 2');
+      playButton.setAttribute('onclick', 'diceRoll();');
+      console.log('Play button onclick attribute set to dice roll ');
+    }else {
+      activePlayer = counter1;
+      console.log('switching to Player 1');
+      playButton.setAttribute('onclick', 'diceRoll();');
+      console.log('Play button onclick attribute set to dice roll');
+    }
 
-   }
- }
+    clearInterval(interva);
+  }
+}
